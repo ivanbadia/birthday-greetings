90a1f075aba2a16a9198d91b00d5f94c16f805fd
kyoku <ivanbadia@gmail.com>
2017-07-09 12:32:50 +0200

    refactor file employee repository

--- src/main/java/infrastructure/persistence/FileEmployeeRepository.java
+++ src/main/java/infrastructure/persistence/FileEmployeeRepository.java
@@ -12,16 +12,25 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * Created by Ivan on 09/07/2017.
- */
 public class FileEmployeeRepository implements EmployeeRepository{
     private static final DateTimeFormatter BIRTH_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    public static final int FIRST_NAME_POSITION = 1;
+    public static final int LAST_NAME_POSITION = 0;
+    public static final int BIRTH_DATE_POSITION = 2;
+    public static final int EMAIL_POSITION = 3;
 
     private final String file;
+
+    private Function<String, Employee> parseEmployee  = line -> {
+        List<String> employeeValues = Arrays.stream(line.split(","))
+                                    .map(String::trim)
+                                    .collect(Collectors.toList());
+        return new Employee(employeeValues.get(FIRST_NAME_POSITION), employeeValues.get(LAST_NAME_POSITION), LocalDate.parse(employeeValues.get(BIRTH_DATE_POSITION), BIRTH_DATE_FORMAT), employeeValues.get(EMAIL_POSITION));
+    };
 
     public FileEmployeeRepository(String file) {
         this.file = file;
@@ -30,13 +39,9 @@ public class FileEmployeeRepository implements EmployeeRepository{
     @Override
     public List<Employee> findEmployeesBornOn(MonthDay monthDay) {
         try (Stream<String> stream = Files.lines(Paths.get(file)).skip(1)) {
-            return stream.map(line -> {
-                List<String> values = Arrays.stream(line.split(",")).map(String::trim).collect(Collectors.toList());
-                return new Employee(values.get(1), values.get(0),
-                        LocalDate.parse(values.get(2), BIRTH_DATE_FORMAT), values.get(3));
-            }).filter(employee -> employee.birthdayIsOn(monthDay))
+            return stream.map(parseEmployee)
+                    .filter(employee -> employee.birthdayIsOn(monthDay))
                     .collect(Collectors.toList());
-
         } catch (IOException e) {
             e.printStackTrace();
             return new ArrayList<>();
--- src/test/java/infrastructure/persistence/FileEmployeeRepositoryIT.java
+++ src/test/java/infrastructure/persistence/FileEmployeeRepositoryIT.java
@@ -21,10 +21,7 @@ public class FileEmployeeRepositoryIT {
     @Test
     public void should_return_employees_born_on_a_given_date() throws URISyntaxException {
         //Given
-        URI uri = getClass().getClassLoader().getResource("employees.csv").toURI();
-        String file = Paths.get(uri).toString();
-
-        FileEmployeeRepository repository = new FileEmployeeRepository(file);
+        FileEmployeeRepository repository = new FileEmployeeRepository(employeesFile());
 
         //When
         List<Employee> employees = repository.findEmployeesBornOn(MonthDay.of(10, 8));
@@ -37,5 +34,10 @@ public class FileEmployeeRepositoryIT {
                     EmployeeBuilder.builder().withLastName("Garcia").withFirstName("Pedro").withDateOfBirth(LocalDate.of(1980, 10, 8)).withEmail("pedro.garcia@foobar.com").build()
                     );
 
+    }
+
+    private String employeesFile() throws URISyntaxException {
+        URI uri = getClass().getClassLoader().getResource("employees.csv").toURI();
+        return Paths.get(uri).toString();
     }
 }
