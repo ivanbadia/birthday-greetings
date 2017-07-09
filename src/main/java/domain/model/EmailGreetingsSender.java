c746e5aef19cd45af52842cfe20bb67d31e97da8
kyoku <ivanbadia@gmail.com>
2017-07-09 10:48:51 +0200

    send greetings email test in green

--- src/main/java/domain/model/EmailGreetingsSender.java
+++ src/main/java/domain/model/EmailGreetingsSender.java
@@ -1,11 +1,24 @@
 package domain.model;
 
+import infrastructure.email.Email;
+import infrastructure.email.EmailSender;
+
 /**
  * Created by Ivan on 09/07/2017.
  */
 public class EmailGreetingsSender implements GreetingsSender {
+    private EmailSender emailSender;
+
+    public EmailGreetingsSender(EmailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
     @Override
     public void sendGreetingsTo(Employee employee) {
+        emailSender.send(greetingsEmailFor(employee));
+    }
 
+    private Email greetingsEmailFor(Employee employee) {
+        return new Email(employee.getEmail(), "Happy birthday!", String.format("Happy birthday, dear %s!", employee.getFirstName()));
     }
 }
